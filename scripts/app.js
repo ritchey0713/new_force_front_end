9e1ca893f4d0dc50acebd8a2b0287aaf6040d26e
Dj <ritchey0713@gmail.com>
2019-10-23 16:21:32 -0400

    refactor app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,58 +1,12 @@
 // global team members 
 let allMembers = []
 
+const teamIndexUrl = "http://localhost:3000/team_members"
+
 let main = document.querySelector("#main")
 
-
-fetch("http://localhost:3000/team_members")
-.then(resp => resp.json())
-.then(json => createObjs(json))
-
-const createObjs = (json) => {
-    json.data.forEach((team_member) => {
-        let teamMember = new TeamMember(team_member)
-        let seasons = json.included.filter((season) => {
-            return season.relationships.team_member.data.id === team_member.id
-        })
-        createSeasons(teamMember, seasons)
-        // teamMember.seasons = seasons
-        allMembers.push(teamMember)
-    })
-    generateTeamList(allMembers)
-}
-
-const createSeasons = (teamMember, seasons) => {
-    seasons.forEach((season) => {
-        let seasonObj = new Season(season)
-        teamMember.seasons.push(seasonObj)
-    })
-}
+fetchAllMembers(teamIndexUrl)
 
 
-const generateTeamList = (allMembers) => {
-    const containerEl = document.createElement("div")
-    const listEl = document.createElement("ul")
-    
-
-    containerEl.classList.add("all-members")
-    listEl.classList.add("list")
-    
-    if (allMembers.length > 0){
-        allMembers.forEach((member => {
-            const itemEl = document.createElement("li")
-            itemEl.classList.add(`team-member-${member.uplay}`)
-            // renderTeam(member)
-            itemEl.textContent = member.uplay
-            listEl.appendChild(itemEl)
-        }))
-
-    } else {
-        containerEl.textContent = "Oops try again later!"
-    }
-
-    containerEl.appendChild(listEl)
-    main.append(containerEl)
-    
-}
 
 
