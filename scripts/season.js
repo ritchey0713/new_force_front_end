599e76af57690e140c0761458906e7a091c26f07
Dj <ritchey0713@gmail.com>
2019-10-23 15:59:41 -0400

    add season class

---
+++ scripts/season.js
@@ -0,0 +1,10 @@
+class Season {
+    constructor(season){
+        this.id = season.id
+        this.mmr = season.attributes.mmr
+        this.kda = season.attributes.kda
+        this.wl = season.attributes.wl 
+        this.headShotRatio = season.attributes.headshot_ratio
+        this.teammemberId = season.relationships.team_member.data.id
+    }
+}
